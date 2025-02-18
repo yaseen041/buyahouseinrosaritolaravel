56c01fc437d4e97c77ffa6fb06f89d604107aa7d
Ishtiaq Amjad <muhammadishtiaqamjad@gmail.com>
2025-02-18 10:39:27 +0500

    //

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -6,92 +6,93 @@
 					<a href="{{ url('/')}}"><img src="{{ asset('assets/img') }}/{{ get_section_content('project', 'site_logo') }}" data-sticky-logo="images/logo-red.svg" alt=""></a>
 				</div>
 				<div class="triger-div">
-				<div class="mmenu-trigger">
-					<button class="hamburger hamburger--collapse" type="button">
-						<span class="hamburger-box">
-							<span class="hamburger-inner"></span>
-						</span>
-					</button>
-				</div>
+					<div class="mmenu-trigger">
+						<button class="hamburger hamburger--collapse" type="button">
+							<span class="hamburger-box">
+								<span class="hamburger-inner"></span>
+							</span>
+						</button>
+					</div>
 				</div>
 				<nav id="navigation" class="style-1 white">
 					<ul id="responsive">
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+								Menu
+							</a>
+							<div class="dropdown-menu mega-menu" aria-labelledby="navbarDropdown">
+								<div class="container">
+									<div class="row">
+										<!-- Column 1 -->
+										<div class="col-md-2">
+											<img src="{{ asset('assets/icons/building.png') }}" width="40" alt="icon">
+											<h5> Rosarito Area Real Estate</h5>
+											<ul>
+												<li><i class="fa fa-home"></i> Highly Rated Comunities With Noteable  Expat Populations</li>
+											</ul>
+										</div>
+										<!-- Column 2 -->
+										<div class="col-md-2">
+											<img src="{{ asset('assets/icons/shield.png') }}" width="40" alt="icon">
+											<h5>Safety & Security</h5>
+											<ul>
+												<li><i class="fa fa-user"></i> Sefest Areas & </li>
+												<li><i class="fa fa-cog"></i> Settings</li>
+												<li><i class="fa fa-question-circle"></i> Help</li>
+												<li><i class="fa fa-book"></i> Tutorials</li>
+												<li><i class="fa fa-paper-plane"></i> Projects</li>
+											</ul>
+										</div>
+										<!-- Column 3 -->
+										<div class="col-md-2">
+											<img src="{{ asset('assets/icons/wallet.png') }}" width="40" alt="icon">
+											<h5> Cos of Living & Daily Expenses</h5>
+											<ul>
+												<li><i class="fa fa-envelope"></i> Email</li>
+												<li><i class="fa fa-phone"></i> Contact</li>
+												<li><i class="fa fa-map-marker"></i> Location</li>
+												<li><i class="fa fa-calendar"></i> Calendar</li>
+												<li><i class="fa fa-tasks"></i> Tasks</li>
+											</ul>
+										</div>
+										<!-- Column 4 -->
+										<div class="col-md-2">
+											<img src="{{ asset('assets/icons/tree.png') }}" width="40" alt="icon">
+											<h5> Recreation, Lifestyle & Fun</h5>
+											<ul>
+												<li><i class="fa fa-gear"></i> Settings</li>
+												<li><i class="fa fa-comments"></i> Feedback</li>
+												<li><i class="fa fa-cloud"></i> Cloud</li>
+												<li><i class="fa fa-rocket"></i> Launch</li>
+												<li><i class="fa fa-clock"></i> Time</li>
+											</ul>
+										</div>
+										<!-- Column 5 -->
+										<div class="col-md-2">
+											<img src="{{ asset('assets/icons/car.png') }}" width="40" alt="icon">
+											<h5>Transportation, Travel & Banking</h5>
+											<ul>
+												<li><i class="fa fa-cart-plus"></i> Cart</li>
+												<li><i class="fa fa-gift"></i> Offers</li>
+												<li><i class="fa fa-map"></i> Locations</li>
+												<li><i class="fa fa-bookmark"></i> Bookmarks</li>
+												<li><i class="fa fa-globe"></i> World</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
 						<li><a href="{{ url('/')}}">Home</a></li>
 						<li><a href="{{ url('properties')}}">Property</a></li>
 						<li><a href="{{ url('about')}}">About</a></li>
 						<li><a href="{{ url('blog')}}">Blog</a></li>
 						<li><a href="{{ url('faq')}}">FAQ</a></li>
 						<li><a href="{{ url('contact')}}">Contact</a></li>
-						<li class="nav-item dropdown">
-    <a href="#" class="nav-link dropdown-toggle" id="productsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Products
-        <i class="flaticon-arrow-down-sign-to-navigate"></i>
-    </a>
-    <div class="dropdown-menu" aria-labelledby="productsDropdown">
-        <a class="dropdown-item" href="https://dokan.co/wordpress/" target="_blank" rel="noopener noreferrer">
-            <img src="/img/Dokan.svg" alt="Dokan Multivendor" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">Dokan Multivendor</span>
-            <p>Build your dream multi vendor marketplace</p>
-        </a>
-        <a class="dropdown-item" href="/wp-user-frontend-pro/">
-            <img src="/img/User-Frontend.svg" alt="WP User Frontend Pro" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">WP User Frontend Pro</span>
-            <p>Ultimate Frontend Solution for WordPress</p>
-        </a>
-        <a class="dropdown-item" href="https://happyaddons.com/?utm_source=wedevs&utm_medium=menu" target="_blank" rel="noopener noreferrer">
-            <img src="/img/happy-addons.png" alt="Happy Addons" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">Happy Addons</span>
-            <p>Powerful elementor widgets to create websites</p>
-        </a>
-        <a class="dropdown-item" href="/wp-project-manager-pro/">
-            <img src="/img/Project-Manager.svg" alt="WP Project Manager Pro" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">WP Project Manager Pro</span>
-            <p>Project Management tool for your team</p>
-        </a>
-        <a class="dropdown-item" href="https://getwemail.io/?utm_source=wedevs&utm_medium=menu" target="_blank" rel="noopener noreferrer">
-            <img src="/img/wemail.svg" alt="weMail" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">weMail</span>
-            <p>Make Email Marketing simplified with WordPress</p>
-        </a>
-        <a class="dropdown-item" href="https://wperp.com/?utm_source=wedevs&utm_medium=menu" target="_blank" rel="noopener noreferrer">
-            <img src="/img/ERP.svg" alt="WP ERP" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">WP ERP</span>
-            <p>Automate your business or company operation</p>
-        </a>
-        <a class="dropdown-item" href="https://appsero.com/?utm_source=wedevs&utm_medium=menu" target="_blank" rel="noopener noreferrer">
-            <img src="/img/appsero-icon.svg" alt="Appsero" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">Appsero</span>
-            <p>WP Analytics, Licensing & Deployment Tool</p>
-        </a>
-        <a class="dropdown-item" href="https://dokan.co/wordpress/wepos/" target="_blank" rel="noopener noreferrer">
-            <img src="/img/wepos.svg" alt="wePOS" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">wePOS</span>
-            <p>Fastest POS System for WooCommerce</p>
-        </a>
-        <a class="dropdown-item" href="https://wedocs.co/?utm_source=weDevs&utm_medium=Mega+Menu&utm_campaign=Internal+linking" target="_blank" rel="noopener noreferrer">
-            <img src="/img/wedocs.svg" alt="weDocs" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">weDocs <span class="badge badge-pill badge-danger">New</span></span>
-            <p>Create great-looking documentation for your products</p>
-        </a>
-        <a class="dropdown-item" href="https://flywp.com/?utm_source=weDevs&utm_medium=Mega+Menu&utm_campaign=Internal+linking" target="_blank" rel="noopener noreferrer">
-            <img src="/img/flywp.svg" alt="FlyWP" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">FlyWP <span class="badge badge-pill badge-danger">New</span></span>
-            <p>Deploy WordPress sites on your own server in minutes</p>
-        </a>
-        <a class="dropdown-item" href="https://inboxwp.com/?utm_source=weDevs&utm_medium=Mega+Menu&utm_campaign=Internal+linking" target="_blank" rel="noopener noreferrer">
-            <img src="/img/inboxwp.svg" alt="InboxWP" width="35" height="35" class="mr-2">
-            <span class="font-weight-bold">InboxWP <span class="badge badge-pill badge-danger">New</span></span>
-            <p>Ensure all your mission-critical emails are delivered on time</p>
-        </a>
-        <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="/products/">
-            View all products <i class="flaticon-arrow-pointing-to-right"></i>
-        </a>
-    </div>
-</li>
-
 					</ul>
 				</nav>
+
+
 
 
 
